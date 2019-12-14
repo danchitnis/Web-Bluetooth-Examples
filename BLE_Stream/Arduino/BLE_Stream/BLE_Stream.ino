d0861d735fe7ef633cf191971b24aa9bb31e7179
danchitnis <danchitnis@users.noreply.github.com>
2019-12-14 01:34:32 +0000

    Streaming data
    Notify and subscribe services

---
+++ BLE_Stream/Arduino/BLE_Stream/BLE_Stream.ino
@@ -0,0 +1,119 @@
+/*
+  Battery Monitor
+
+  This example creates a BLE peripheral with the standard battery service and
+  level characteristic. The A0 pin is used to calculate the battery level.
+
+  The circuit:
+  - Arduino MKR WiFi 1010, Arduino Uno WiFi Rev2 board, Arduino Nano 33 IoT,
+    Arduino Nano 33 BLE, or Arduino Nano 33 BLE Sense board.
+
+  You can use a generic BLE central app, like LightBlue (iOS and Android) or
+  nRF Connect (Android), to interact with the services and characteristics
+  created in this sketch.
+
+  This example code is in the public domain.
+*/
+
+#include <ArduinoBLE.h>
+
+ // BLE Battery Service
+BLEService batteryService("f8c4fc40-e7d3-4e81-b686-9be0160b9b7d");
+
+// BLE Battery Level Characteristic
+BLEUnsignedCharCharacteristic batteryLevelChar("f8c4fc41-e7d3-4e81-b686-9be0160b9b7d", BLERead | BLEWrite | BLENotify | BLEIndicate);
+
+int oldBatteryLevel = 0;  // last battery level reading from analog input
+long previousMillis = 0;  // last time the battery level was checked, in ms
+int counter = 0;
+
+bool LED = false;
+
+void setup() {
+  Serial.begin(9600);    // initialize serial communication
+  // while (!Serial);
+  wait_ms(500);
+
+  pinMode(LED_BUILTIN, OUTPUT); // initialize the built-in LED pin to indicate when a central is connected
+
+  // begin initialization
+  if (!BLE.begin()) {
+    Serial.println("starting BLE failed!");
+
+    while (1);
+  }
+
+  /* Set a local name for the BLE device
+     This name will appear in advertising packets
+     and can be used by remote devices to identify this BLE device
+     The name can be changed but maybe be truncated based on space left in advertisement packet
+  */
+  BLE.setLocalName("BLE Nano 33");
+  BLE.setAdvertisedService(batteryService); // add the service UUID
+  batteryService.addCharacteristic(batteryLevelChar); // add the battery level characteristic
+  BLE.addService(batteryService); // Add the battery service
+  batteryLevelChar.writeValue(oldBatteryLevel); // set initial value for this characteristic
+
+  /* Start advertising BLE.  It will start continuously transmitting BLE
+     advertising packets and will be visible to remote BLE central devices
+     until it receives a new connection */
+
+  // start advertising
+  BLE.advertise();
+
+  Serial.println("Bluetooth device active, waiting for connections...");
+}
+
+void loop() {
+  // wait for a BLE central
+  BLEDevice central = BLE.central();
+
+  // if a central is connected to the peripheral:
+  if (central) {
+    Serial.print("Connected to central: ");
+    // print the central's BT address:
+    Serial.println(central.address());
+    // turn on the LED to indicate the connection:
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    // check the battery level every 200ms
+    // while the central is connected:
+    while (central.connected()) {
+      long currentMillis = millis();
+      // if 200ms have passed, check the battery level:
+      if (currentMillis - previousMillis >= 100) {
+        previousMillis = currentMillis;
+        updateBatteryLevel();
+        digitalWrite(LED_BUILTIN, toggleLED());
+      }
+    }
+    // when the central disconnects, turn off the LED:
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.print("Disconnected from central: ");
+    Serial.println(central.address());
+  }
+}
+
+void updateBatteryLevel() {
+  /* Read the current voltage level on the A0 analog input pin.
+     This is used here to simulate the charge level of a battery.
+  */
+  int battery = counter++;
+  int batteryLevel = map(battery, 0, 1023, 0, 100);
+
+  if (batteryLevel != oldBatteryLevel) {      // if the battery level has changed
+    Serial.print("Battery Level % is now: "); // print it
+    Serial.println(batteryLevel);
+    batteryLevelChar.writeValue(batteryLevel);  // and update the battery level characteristic
+    oldBatteryLevel = batteryLevel;           // save the level for next comparison
+  }
+}
+
+int toggleLED() {
+  int LEDint = 0;
+  if (LED) LEDint = 1;
+  else LEDint = 0;
+  LED = !LED;
+
+  return LEDint;
+}
---
+++ BLE_Stream/src/BLE_Stream.ts
@@ -0,0 +1,112 @@
+/**
+ * WebBlutooth example for Arduino Nano BLE 33
+ *
+ * Author: Danial Chitnis
+ * December 2019
+ *
+ * Please upload the sketch before running this code
+ */
+
+ let LED = false;
+ let bleChar: BluetoothRemoteGATTCharacteristic;
+
+ let btConnect = document.getElementById("btConnect") as HTMLButtonElement;
+ let btStart = document.getElementById("btStart") as HTMLButtonElement;
+ let btStop = document.getElementById("btStop") as HTMLButtonElement;
+
+ let pLog = document.getElementById("pLog") as HTMLParagraphElement;
+
+
+ btConnect.addEventListener("click", () => {
+
+
+    /**
+     * these should match the UUID in your Arduino sketch. you can generate your own UUIDs here:
+     * https://www.uuidgenerator.net/
+     */
+    const serviceUuid = "f8c4fc40-e7d3-4e81-b686-9be0160b9b7d" as BluetoothServiceUUID;
+    const characteristicUuid = "f8c4fc41-e7d3-4e81-b686-9be0160b9b7d" as BluetoothCharacteristicUUID;
+
+
+    // The following code section is based on Chrome example
+    // https://googlechrome.github.io/samples/web-bluetooth/characteristic-properties.html
+    log("Requesting Bluetooth Device...");
+    navigator.bluetooth.requestDevice({
+        acceptAllDevices: true,
+        optionalServices: [serviceUuid],
+    })
+    .then( (device) => {
+        log("Connecting to GATT Server...");
+        return device.gatt.connect();
+    })
+    .then( (server) => {
+        log("Getting Service...");
+        return server.getPrimaryService(serviceUuid);
+    })
+    .then( (service) => {
+        log("Getting Characteristic...");
+        return service.getCharacteristic(characteristicUuid);
+    })
+    .then( (characteristic) => {
+        log("> Characteristic UUID:  " + characteristic.uuid);
+        log("> Broadcast:            " + characteristic.properties.broadcast);
+        log("> Read:                 " + characteristic.properties.read);
+        log("> Write w/o response:   " +
+        characteristic.properties.writeWithoutResponse);
+        log("> Write:                " + characteristic.properties.write);
+        log("> Notify:               " + characteristic.properties.notify);
+        log("> Indicate:             " + characteristic.properties.indicate);
+        log("> Signed Write:         " +
+        characteristic.properties.authenticatedSignedWrites);
+        log("> Queued Write:         " + characteristic.properties.reliableWrite);
+        log("> Writable Auxiliaries: " + characteristic.properties.writableAuxiliaries);
+
+        bleChar = characteristic;
+
+    })
+    .catch( (error) => {
+        log("Argh! " + error);
+    });
+ });
+
+
+ btStart.addEventListener("click", () => {
+
+    bleChar.startNotifications().then( () => {
+        log("Notifications started");
+        bleChar.addEventListener("characteristicvaluechanged", handleNotifications);
+    });
+
+ });
+
+ btStop.addEventListener("click", () => {
+    if (bleChar) {
+        bleChar.stopNotifications()
+        .then(_ => {
+          log("Notifications stopped");
+          bleChar.removeEventListener("characteristicvaluechanged", handleNotifications);
+        })
+        .catch( (error) => {
+          log("Argh! " + error);
+        });
+      }
+ });
+
+
+ function handleNotifications(event: Event) {
+    const target = event.target as BluetoothRemoteGATTCharacteristic;
+    const value = target.value;
+    // Convert raw data bytes to hex values just for the sake of showing something.
+    // In the "real" world, you'd use data.getUint8, data.getUint16 or even
+    // TextDecoder to process raw data bytes.
+    for (let i = 0; i < value.byteLength; i++) {
+        log("0x" + ("00" + value.getUint8(i).toString(16)).slice(-2));
+    }
+ }
+
+
+ function log(str: string) {
+    pLog.innerHTML = pLog.innerHTML + "<br>> " + str;
+ }
+
+
