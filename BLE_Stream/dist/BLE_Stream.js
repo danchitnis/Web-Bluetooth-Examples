08b77b3e5657623d0d4ccf9fd52d6b50c16d6c07
Danial Chitnis <danchitnis@users.noreply.github.com>
2019-12-21 04:17:24 +0000

    aync update
    BLE counter

--- BLE_Stream/dist/BLE_Stream.js
+++ BLE_Stream/dist/BLE_Stream.js
@@ -1,3 +1,12 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 /**
  * WebBlutooth example for Arduino Nano BLE 33
  *
@@ -6,84 +15,96 @@
  *
  * Please upload the sketch before running this code
  */
-var LED = false;
-var bleChar;
-var btConnect = document.getElementById("btConnect");
-var btStart = document.getElementById("btStart");
-var btStop = document.getElementById("btStop");
-var pLog = document.getElementById("pLog");
-btConnect.addEventListener("click", function () {
-    /**
-     * these should match the UUID in your Arduino sketch. you can generate your own UUIDs here:
-     * https://www.uuidgenerator.net/
-     */
-    var serviceUuid = "f8c4fc40-e7d3-4e81-b686-9be0160b9b7d";
-    var characteristicUuid = "f8c4fc41-e7d3-4e81-b686-9be0160b9b7d";
-    // The following code section is based on Chrome example
-    // https://googlechrome.github.io/samples/web-bluetooth/characteristic-properties.html
-    log("Requesting Bluetooth Device...");
-    navigator.bluetooth.requestDevice({
-        acceptAllDevices: true,
-        optionalServices: [serviceUuid]
-    })
-        .then(function (device) {
-        log("Connecting to GATT Server...");
-        return device.gatt.connect();
-    })
-        .then(function (server) {
-        log("Getting Service...");
-        return server.getPrimaryService(serviceUuid);
-    })
-        .then(function (service) {
-        log("Getting Characteristic...");
-        return service.getCharacteristic(characteristicUuid);
-    })
-        .then(function (characteristic) {
-        log("> Characteristic UUID:  " + characteristic.uuid);
-        log("> Broadcast:            " + characteristic.properties.broadcast);
-        log("> Read:                 " + characteristic.properties.read);
-        log("> Write w/o response:   " +
-            characteristic.properties.writeWithoutResponse);
-        log("> Write:                " + characteristic.properties.write);
-        log("> Notify:               " + characteristic.properties.notify);
-        log("> Indicate:             " + characteristic.properties.indicate);
-        log("> Signed Write:         " +
-            characteristic.properties.authenticatedSignedWrites);
-        log("> Queued Write:         " + characteristic.properties.reliableWrite);
-        log("> Writable Auxiliaries: " + characteristic.properties.writableAuxiliaries);
-        bleChar = characteristic;
-    })["catch"](function (error) {
-        log("Argh! " + error);
+{
+    //let LED = false;
+    let bleChar;
+    const btConnect = document.getElementById("btConnect");
+    const btStart = document.getElementById("btStart");
+    const btStop = document.getElementById("btStop");
+    const pLog = document.getElementById("pLog");
+    const serviceUuid = "f8c4fc40-e7d3-4e81-b686-9be0160b9b7d";
+    const characteristicUuid = "f8c4fc41-e7d3-4e81-b686-9be0160b9b7d";
+    btConnect.addEventListener("click", () => {
+        /**
+         * these should match the UUID in your Arduino sketch. you can generate your own UUIDs here:
+         * https://www.uuidgenerator.net/
+         */
+        // The following code section is based on Chrome example
+        // https://googlechrome.github.io/samples/web-bluetooth/characteristic-properties.html
+        clikConnect();
     });
-});
-btStart.addEventListener("click", function () {
-    bleChar.startNotifications().then(function () {
-        log("Notifications started");
-        bleChar.addEventListener("characteristicvaluechanged", handleNotifications);
+    btStart.addEventListener("click", () => {
+        startNotifications();
     });
-});
-btStop.addEventListener("click", function () {
-    if (bleChar) {
-        bleChar.stopNotifications()
-            .then(function (_) {
-            log("Notifications stopped");
-            bleChar.removeEventListener("characteristicvaluechanged", handleNotifications);
-        })["catch"](function (error) {
-            log("Argh! " + error);
+    btStop.addEventListener("click", () => {
+        if (bleChar) {
+            bleChar.stopNotifications()
+                .then(_ => {
+                log("Notifications stopped");
+                bleChar.removeEventListener("characteristicvaluechanged", handleNotifications);
+            })
+                .catch((error) => {
+                log("Argh! " + error);
+            });
+        }
+    });
+    function startNotifications() {
+        return __awaiter(this, void 0, void 0, function* () {
+            log("Notifications started");
+            yield bleChar.startNotifications();
+            bleChar.addEventListener("characteristicvaluechanged", handleNotifications);
         });
     }
-});
-function handleNotifications(event) {
-    var target = event.target;
-    var value = target.value;
-    // Convert raw data bytes to hex values just for the sake of showing something.
-    // In the "real" world, you'd use data.getUint8, data.getUint16 or even
-    // TextDecoder to process raw data bytes.
-    for (var i = 0; i < value.byteLength; i++) {
-        log("0x" + ("00" + value.getUint8(i).toString(16)).slice(-2));
+    function handleNotifications(event) {
+        const target = event.target;
+        const value = target.value;
+        // Convert raw data bytes to hex values just for the sake of showing something.
+        // In the "real" world, you'd use data.getUint8, data.getUint16 or even
+        // TextDecoder to process raw data bytes.
+        for (let i = 0; i < value.byteLength; i++) {
+            log("0x" + ("00" + value.getUint8(i).toString(16)).slice(-2));
+        }
     }
-}
-function log(str) {
-    pLog.innerHTML = pLog.innerHTML + "<br>> " + str;
+    function log(str) {
+        pLog.innerHTML = pLog.innerHTML + "<br>> " + str;
+    }
+    function clikConnect() {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                yield connect();
+            }
+            catch (error) {
+                log("Error ☹: " + error);
+            }
+        });
+    }
+    function connect() {
+        return __awaiter(this, void 0, void 0, function* () {
+            log("Requesting Bluetooth Device...");
+            const bleDevice = yield navigator.bluetooth.requestDevice({
+                acceptAllDevices: true,
+                optionalServices: [serviceUuid],
+            });
+            log("Connecting to GATT Server...");
+            const server = yield bleDevice.gatt.connect();
+            log("Getting Service...");
+            const service = yield server.getPrimaryService(serviceUuid);
+            log("Getting Characteristic...");
+            const characteristic = yield service.getCharacteristic(characteristicUuid);
+            log("> Characteristic UUID:  " + characteristic.uuid);
+            log("> Broadcast:            " + characteristic.properties.broadcast);
+            log("> Read:                 " + characteristic.properties.read);
+            log("> Write w/o response:   " +
+                characteristic.properties.writeWithoutResponse);
+            log("> Write:                " + characteristic.properties.write);
+            log("> Notify:               " + characteristic.properties.notify);
+            log("> Indicate:             " + characteristic.properties.indicate);
+            log("> Signed Write:         " +
+                characteristic.properties.authenticatedSignedWrites);
+            log("> Queued Write:         " + characteristic.properties.reliableWrite);
+            log("> Writable Auxiliaries: " + characteristic.properties.writableAuxiliaries);
+            bleChar = characteristic;
+        });
+    }
 }
 //# sourceMappingURL=BLE_Stream.js.map
--- BLE_Stream/src/BLE_Stream.ts
+++ BLE_Stream/src/BLE_Stream.ts
@@ -7,14 +7,19 @@
  * Please upload the sketch before running this code
  */
 
- let LED = false;
+{
+
+ //let LED = false;
  let bleChar: BluetoothRemoteGATTCharacteristic;
 
- let btConnect = document.getElementById("btConnect") as HTMLButtonElement;
- let btStart = document.getElementById("btStart") as HTMLButtonElement;
- let btStop = document.getElementById("btStop") as HTMLButtonElement;
+ const btConnect = document.getElementById("btConnect") as HTMLButtonElement;
+ const btStart = document.getElementById("btStart") as HTMLButtonElement;
+ const btStop = document.getElementById("btStop") as HTMLButtonElement;
 
- let pLog = document.getElementById("pLog") as HTMLParagraphElement;
+ const pLog = document.getElementById("pLog") as HTMLParagraphElement;
+
+ const serviceUuid = "f8c4fc40-e7d3-4e81-b686-9be0160b9b7d" as BluetoothServiceUUID;
+ const characteristicUuid = "f8c4fc41-e7d3-4e81-b686-9be0160b9b7d" as BluetoothCharacteristicUUID;
 
 
  btConnect.addEventListener("click", () => {
@@ -24,58 +29,19 @@
      * these should match the UUID in your Arduino sketch. you can generate your own UUIDs here:
      * https://www.uuidgenerator.net/
      */
-    const serviceUuid = "f8c4fc40-e7d3-4e81-b686-9be0160b9b7d" as BluetoothServiceUUID;
-    const characteristicUuid = "f8c4fc41-e7d3-4e81-b686-9be0160b9b7d" as BluetoothCharacteristicUUID;
+    
 
 
     // The following code section is based on Chrome example
     // https://googlechrome.github.io/samples/web-bluetooth/characteristic-properties.html
-    log("Requesting Bluetooth Device...");
-    navigator.bluetooth.requestDevice({
-        acceptAllDevices: true,
-        optionalServices: [serviceUuid],
-    })
-    .then( (device) => {
-        log("Connecting to GATT Server...");
-        return device.gatt.connect();
-    })
-    .then( (server) => {
-        log("Getting Service...");
-        return server.getPrimaryService(serviceUuid);
-    })
-    .then( (service) => {
-        log("Getting Characteristic...");
-        return service.getCharacteristic(characteristicUuid);
-    })
-    .then( (characteristic) => {
-        log("> Characteristic UUID:  " + characteristic.uuid);
-        log("> Broadcast:            " + characteristic.properties.broadcast);
-        log("> Read:                 " + characteristic.properties.read);
-        log("> Write w/o response:   " +
-        characteristic.properties.writeWithoutResponse);
-        log("> Write:                " + characteristic.properties.write);
-        log("> Notify:               " + characteristic.properties.notify);
-        log("> Indicate:             " + characteristic.properties.indicate);
-        log("> Signed Write:         " +
-        characteristic.properties.authenticatedSignedWrites);
-        log("> Queued Write:         " + characteristic.properties.reliableWrite);
-        log("> Writable Auxiliaries: " + characteristic.properties.writableAuxiliaries);
-
-        bleChar = characteristic;
-
-    })
-    .catch( (error) => {
-        log("Argh! " + error);
-    });
+    
+    clikConnect();
  });
 
 
  btStart.addEventListener("click", () => {
 
-    bleChar.startNotifications().then( () => {
-        log("Notifications started");
-        bleChar.addEventListener("characteristicvaluechanged", handleNotifications);
-    });
+    startNotifications();
 
  });
 
@@ -92,8 +58,13 @@
       }
  });
 
+ async function startNotifications(): Promise<void> {
+    log("Notifications started"); 
+    await bleChar.startNotifications();
+    bleChar.addEventListener("characteristicvaluechanged", handleNotifications);
+ }
 
- function handleNotifications(event: Event) {
+ function handleNotifications(event: Event): void {
     const target = event.target as BluetoothRemoteGATTCharacteristic;
     const value = target.value;
     // Convert raw data bytes to hex values just for the sake of showing something.
@@ -105,8 +76,51 @@
  }
 
 
- function log(str: string) {
+ function log(str: string): void {
     pLog.innerHTML = pLog.innerHTML + "<br>> " + str;
  }
 
+ async function clikConnect(): Promise<void> {
+    try {
+        await connect();
+    }
+    catch (error) {
+        log("Error ☹: " + error);
+    }
+    
+ }
 
+ async function connect(): Promise<void> {
+    log("Requesting Bluetooth Device...");
+
+
+    const bleDevice = await navigator.bluetooth.requestDevice({
+        acceptAllDevices: true,
+        optionalServices: [serviceUuid],
+    });
+    log("Connecting to GATT Server...");
+    const server = await bleDevice.gatt.connect();
+
+    log("Getting Service...");
+    const service = await server.getPrimaryService(serviceUuid);
+
+    log("Getting Characteristic...");
+    const characteristic = await service.getCharacteristic(characteristicUuid);
+
+    log("> Characteristic UUID:  " + characteristic.uuid);
+    log("> Broadcast:            " + characteristic.properties.broadcast);
+    log("> Read:                 " + characteristic.properties.read);
+    log("> Write w/o response:   " +
+    characteristic.properties.writeWithoutResponse);
+    log("> Write:                " + characteristic.properties.write);
+    log("> Notify:               " + characteristic.properties.notify);
+    log("> Indicate:             " + characteristic.properties.indicate);
+    log("> Signed Write:         " +
+    characteristic.properties.authenticatedSignedWrites);
+    log("> Queued Write:         " + characteristic.properties.reliableWrite);
+    log("> Writable Auxiliaries: " + characteristic.properties.writableAuxiliaries);
+
+    bleChar = characteristic;
+ }
+
+}
